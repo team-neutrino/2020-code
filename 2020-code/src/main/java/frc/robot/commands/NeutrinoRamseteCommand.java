93b2d8a74cf493914491f945ee2809a0afb0c482
Leslie714 <882531kim@ames.k12.ia.us>
2020-01-11 12:11:29 -0600

    Made a ramsete command
    First iteration of the ramsete command.

---
+++ 2020-code/src/main/java/frc/robot/commands/NeutrinoRamseteCommand.java
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+
+public class NeutrinoRamseteCommand extends CommandBase {
+  /**
+   * Creates a new NeutrinoRamseteCommand.
+   */
+
+   private final DriveSubsystem m_Drive;
+   private Trajectory m_Trajectory;
+   private RamseteCommand m_RamseteCommand;
+
+  public NeutrinoRamseteCommand(DriveSubsystem pDrive, Trajectory pTrajectory) {
+    m_Drive = pDrive;
+    addRequirements(m_Drive);
+
+    m_Trajectory = pTrajectory;
+
+    m_RamseteCommand = new RamseteCommand(
+        m_Trajectory,
+        m_Drive::getPose,
+        new RamseteController(DriveConstants.kRamseteB, DriveConstants.kRamseteZeta),
+        new SimpleMotorFeedforward(DriveConstants.ksVolts,
+                                    DriveConstants.kvVoltSecondsPerMeter,
+                                    DriveConstants.kaVoltSecondsSquaredPerMeter),
+        DriveConstants.kDriveKinematics,
+        m_Drive::getWheelSpeeds,
+        new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        // RamseteCommand passes volts to the callback
+        m_Drive::tankDriveVolts,
+        m_Drive
+    );
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_RamseteCommand.initialize();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
