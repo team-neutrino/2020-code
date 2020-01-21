1bd10f3ca6b4918c83cafa34116f874416961b70
Indelisio Scheel Prieto <881619pri@ames.k12.ia.us>
2020-01-20 19:28:55 -0600

    Formatting

--- 2020-code/src/main/java/frc/robot/RobotContainer.java
+++ 2020-code/src/main/java/frc/robot/RobotContainer.java
@@ -33,77 +33,72 @@ import frc.robot.Trajectories.ExampleTrajectory;
 import frc.robot.commands.DriveDataCommand;
 import frc.robot.commands.IntakeDataCommand;
 
-
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
-public class RobotContainer 
-{
-    // The robot's subsystems and commands are defined here...
-    private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-    public final DriveSubsystem m_Drive = new DriveSubsystem();
-    public final IntakeSubsystem m_Intake = new IntakeSubsystem();
+  public final DriveSubsystem m_Drive = new DriveSubsystem();
+  public final IntakeSubsystem m_Intake = new IntakeSubsystem();
 
-    public Joystick m_leftJoystick = new Joystick(Constants.JoystickConstants.LEFT_JOYSTICK_PORT);
-    public Joystick m_rightJoystick = new Joystick(Constants.JoystickConstants.RIGHT_JOYSTICK__PORT);
-    XboxController m_OperatorController = new XboxController(ControllerPorts.XBOX_CONTROLLER_PORT);
-    JoystickButton m_A = new JoystickButton(m_OperatorController, Button.kA.value);
-    JoystickButton m_B = new JoystickButton(m_OperatorController, Button.kB.value);
-    JoystickButton m_X = new JoystickButton(m_OperatorController, Button.kX.value);
-    private final Trajectory m_Trajectory = ExampleTrajectory.exampleTraj;
-    private final NeutrinoRamseteCommand m_autoCommand = new NeutrinoRamseteCommand(m_Drive, m_Trajectory);
-    private final IntakeDataCommand m_intakeData = new IntakeDataCommand(m_Intake);
+  public Joystick m_leftJoystick = new Joystick(Constants.JoystickConstants.LEFT_JOYSTICK_PORT);
+  public Joystick m_rightJoystick = new Joystick(Constants.JoystickConstants.RIGHT_JOYSTICK__PORT);
+  XboxController m_OperatorController = new XboxController(ControllerPorts.XBOX_CONTROLLER_PORT);
+  JoystickButton m_A = new JoystickButton(m_OperatorController, Button.kA.value);
+  JoystickButton m_B = new JoystickButton(m_OperatorController, Button.kB.value);
+  JoystickButton m_X = new JoystickButton(m_OperatorController, Button.kX.value);
+  private final Trajectory m_Trajectory = ExampleTrajectory.exampleTraj;
+  private final NeutrinoRamseteCommand m_autoCommand = new NeutrinoRamseteCommand(m_Drive, m_Trajectory);
+  private final IntakeDataCommand m_intakeData = new IntakeDataCommand(m_Intake);
 
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    final Command tankDriveCommand = new RunCommand(
+        () -> m_Drive.tankDrive(joystickProcessor(m_leftJoystick.getY()), joystickProcessor(m_rightJoystick.getY())),
+        m_Drive);
+    m_Drive.setDefaultCommand(tankDriveCommand);
+    configureButtonBindings();
+  }
 
-    /**
-     * The container for the robot.  Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer()
-    {
-      final Command tankDriveCommand = new RunCommand(
-        () -> m_Drive.tankDrive(joystickProcessor(m_leftJoystick.getY()), joystickProcessor(m_rightJoystick.getY())), m_Drive);
-      m_Drive.setDefaultCommand(tankDriveCommand);
-      configureButtonBindings();
-    }
+  /**
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    m_A.whenPressed(new IntakeGetBallCommand(m_Intake)).whenReleased(new IntakeRetractCommand(m_Intake));
+    m_B.whenPressed(new IntakeDataCommand(m_Intake));
+    m_X.whenPressed(new DriveDataCommand(m_Drive));
+  }
 
-    /**
-     * Use this method to define your button->command mappings.  Buttons can be created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-     * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() 
-    {
-      m_A.whenPressed(new IntakeGetBallCommand(m_Intake))
-          .whenReleased(new IntakeRetractCommand(m_Intake));
-      m_B.whenPressed(new IntakeDataCommand(m_Intake));
-      m_X.whenPressed(new DriveDataCommand(m_Drive));
-    }
-
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-  public Command getAutonomousCommand()
-    {
-       return m_autoCommand;
-    }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return m_autoCommand;
+  }
 
   /**
    * 
    */
-  private double joystickProcessor(double input)
-  {
-    if(Math.abs(input) > Constants.JoystickConstants.DEADZONE_SIZE)
-    {
+  private double joystickProcessor(double input) {
+    if (Math.abs(input) > Constants.JoystickConstants.DEADZONE_SIZE) {
       double absoluteValue = Math.abs(input);
-      double deadzoneCorrectedAbsoluteValue = (1/(1-Constants.JoystickConstants.DEADZONE_SIZE)) * (absoluteValue - 1.0) + 1.0;
-      return Math.pow(deadzoneCorrectedAbsoluteValue,Constants.JoystickConstants.JOYSTICK_CURVE) * (absoluteValue/input);
+      double deadzoneCorrectedAbsoluteValue = (1 / (1 - Constants.JoystickConstants.DEADZONE_SIZE))
+          * (absoluteValue - 1.0) + 1.0;
+      return Math.pow(deadzoneCorrectedAbsoluteValue, Constants.JoystickConstants.JOYSTICK_CURVE)
+          * (absoluteValue / input);
     } else {
       return 0.0;
     }
