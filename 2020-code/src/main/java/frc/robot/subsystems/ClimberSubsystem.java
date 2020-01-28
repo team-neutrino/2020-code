0713bdebd3f3123a43eebb53bfae3b2bfc1fe71f
gurellia53 <gurellia53@gmail.com>
2020-01-27 16:24:10 -0800

    Spotless gradle (#25)
    * this doesn't work
    
    * spotlessJavaCheck
    
    * ok
    
    * formatter
    
    * this is excellent
    
    * cleanup
    
    * auto format
    
    * apply format
    
    * spotless
    
    * gitattributes
    
    * Revert "spotless"
    
    This reverts commit e96579db74204ad5383b9ddc89ea13032ce80fdd.
    
    * spotless
    
    * ctrl shift i

--- 2020-code/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ 2020-code/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -6,18 +6,22 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimberSubsystem extends SubsystemBase {
-  /**
-   * Creates a new ClimberSubsystem.
-   */
-  public ClimberSubsystem() {
+public class ClimberSubsystem extends SubsystemBase
+{
+    /**
+     * Creates a new ClimberSubsystem.
+     */
+    public ClimberSubsystem()
+    {
 
-  }
-  
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    }
+
+    @Override
+    public void periodic()
+    {
+        // This method will be called once per scheduler run
+    }
 }
