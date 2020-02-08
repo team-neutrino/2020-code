eec77dd6430286ebec4a5c4d15d679bdf7aa77b2
Nitzan Friedberg <nitzan.friedberg@gmail.com>
2020-02-08 11:20:44 -0600

    Implement SetSpeedCommand

--- 2020-code/src/main/java/frc/robot/commands/ShooterSetSpeedCommand.java
+++ 2020-code/src/main/java/frc/robot/commands/ShooterSetSpeedCommand.java
@@ -33,13 +33,14 @@ public class ShooterSetSpeedCommand extends CommandBase
     @Override
     public void execute()
     {
-      
+      m_shooter.setSpeed(20000);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted)
     {
+        m_shooter.setPower(0);
     }
 
     // Returns true when the command should end.
