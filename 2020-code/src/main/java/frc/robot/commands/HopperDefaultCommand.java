442a9635894f15f951eb138d808572e1462c9bac
nitzanfriedberg <51808324+nitzanfriedberg@users.noreply.github.com>
2020-02-15 14:25:48 -0600

    Hopper beam break (#58)
    * Hopper subsystem update
    
    * Commute to update from master
    
    * Commit
    
    * Update from master commit
    
    * Update commit
    
    * Hopper beam break test - successful
    
    * Update from master commit
    
    * update
    
    * Hopper Logic for tower
    
    * Hopper beam break logic works
    
    * Made hopper private
    
    * Bottom Hopper Code Made
    
    * Commit
    
    * Hopper finished!
    
    * Commit to hopper
    
    * Deleted invert_motor_output constant
    
    * Set up hopper logic in periodic()
    
    Need to move to command and make defaultcommand
    
    * Moved out to command
    
    * Inverted motor
    
    * Take out old commented out code
    
    Co-authored-by: 883411 <58920277+883411@users.noreply.github.com>
    Co-authored-by: Indelisio Scheel Prieto <31929596+IndyPrietoSchool@users.noreply.github.com>
    Co-authored-by: davidd23 <59034876+davidd23@users.noreply.github.com>

---
+++ 2020-code/src/main/java/frc/robot/commands/HopperDefaultCommand.java
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.HopperSubsystem;
+
+public class HopperDefaultCommand extends CommandBase
+{
+    /**
+     * Creates a new HopperDefaultCommand.
+     */
+
+    private HopperSubsystem m_HopperSubsystem;
+    boolean bottomBeam;
+    boolean topBeam;
+    public HopperDefaultCommand(HopperSubsystem p_Hopper)
+    {
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(p_Hopper);
+        m_HopperSubsystem = p_Hopper;
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize()
+    {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute()
+    {
+        bottomBeam = m_HopperSubsystem.bottomBeamStatus();
+        topBeam = m_HopperSubsystem.topBeamStatus();
+        SmartDashboard.putBoolean("Beam Break 1", bottomBeam);
+        SmartDashboard.putBoolean("Beam Break 2", topBeam);
+        if (topBeam == false || m_HopperSubsystem.getTime() >= 0.1)
+        {
+            m_HopperSubsystem.stop();
+            System.out.println("stopped");
+            return;
+        }
+        if (bottomBeam == false)
+        {
+            System.out.println("intaking");
+            m_HopperSubsystem.intake();
+            m_HopperSubsystem.stopTimer();
+        }
+        else if (m_HopperSubsystem.getPrevBotBeam() == false && bottomBeam == true)
+        {
+            System.out.println("starting timer");
+            m_HopperSubsystem.resetTimer();
+            m_HopperSubsystem.startTimer();
+        }
+        m_HopperSubsystem.setPrevBotBeam(bottomBeam);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted)
+    {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished()
+    {
+        return false;
+    }
+}
