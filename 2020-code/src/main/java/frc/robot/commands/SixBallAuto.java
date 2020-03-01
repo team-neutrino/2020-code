78d4399cb3d24efe2521646ddcb4b4a93b37cce3
Nitzan Friedberg <nitzan.friedberg@gmail.com>
2020-03-01 10:57:08 -0600

    Update SixBallAuto.java

--- 2020-code/src/main/java/frc/robot/commands/SixBallAuto.java
+++ 2020-code/src/main/java/frc/robot/commands/SixBallAuto.java
@@ -35,32 +35,28 @@ public class SixBallAuto extends SequentialCommandGroup
     public SixBallAuto(ShooterSubsystem p_Shooter, HopperSubsystem p_Hopper, IntakePIDSubsystem p_Intake,
             DriveSubsystem p_Drive, TurretSubsystem p_TurretSubsystem)
     {
-        // Add your commands in the super() call, e.g.
-        // super(new FooCommand(), new BarCommand());
-        super(
-
-        );
         Trajectory trajectory = ExampleTrajectory.sixBall0;
-        Trajectory trajectory1 = ExampleTrajectory.sixBall1;
 
         PIDController leftController = new PIDController(DriveConstants.KP_DRIVE_VEL, 0, 0);
         PIDController rightController = new PIDController(DriveConstants.KP_DRIVE_VEL, 0, 0);
+
         RamseteController controller = new RamseteController(DriveConstants.K_RAMSETE_B, DriveConstants.K_RAMSETE_ZETA);
+
         SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(DriveConstants.KS_VOLTS,
             DriveConstants.KV_VOLT_SECONDS_PER_METER, DriveConstants.KA_VOLT_SECONDS_SQUARED_PER_METER);
 
+        // Six ball auton trajectory
         RamseteCommand sixBallTraj0 = new RamseteCommand(trajectory, p_Drive::getPose, controller, feedforward,
             DriveConstants.K_DRIVE_KINEMATICS, p_Drive::getWheelSpeeds, leftController, rightController,
             p_Drive::tankDriveVolts, p_Drive);
-        RamseteCommand sixBallTraj1 = new RamseteCommand(trajectory1, p_Drive::getPose, controller, feedforward,
-            DriveConstants.K_DRIVE_KINEMATICS, p_Drive::getWheelSpeeds, leftController, rightController,
-            p_Drive::tankDriveVolts, p_Drive);
 
-        addCommands(new TurretSetAngleCommand(p_TurretSubsystem, 45.0).alongWith(
-            new SequentialCommandGroup(new InstantCommand(p_Intake::setArmDown), new WaitCommand(.75),
-                new ShootAuton(p_Shooter, p_Hopper, 3, 80000), new InstantCommand(p_Intake::setIntakeOn, p_Intake),
-                sixBallTraj0, new InstantCommand(() -> p_Drive.tankDrive(0, 0)),
-                new InstantCommand(() -> p_Intake.setAngle(Constants.IntakeConstants.ARM_UP_ANGLE)),
-                new ShootAuton(p_Shooter, p_Hopper, 7, 85000))));
+        addCommands(
+            // TurretSetAngleCommand coexists with the default TurretAimCommand
+            // as a ParallelCommandGroup
+            new TurretSetAngleCommand(p_TurretSubsystem, 45.0).alongWith(
+                new SequentialCommandGroup(new InstantCommand(p_Intake::setArmDown), new WaitCommand(.75),
+                    new ShootAuton(p_Shooter, p_Hopper, 3, 80000), new InstantCommand(p_Intake::setIntakeOn, p_Intake),
+                    sixBallTraj0, new InstantCommand(() -> p_Intake.setAngle(Constants.IntakeConstants.ARM_UP_ANGLE)),
+                    new ShootAuton(p_Shooter, p_Hopper, 7, 85000))));
     }
 }
