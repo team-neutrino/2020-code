eee9376cb2aacb973966a95a4306fb729c8a60da
Andrew Gurik <gurellia53@gmail.com>
2020-03-09 19:04:21 -0500

    add once

--- 2020-code/src/main/java/frc/robot/subsystems/DriverViewSubsystem.java
+++ 2020-code/src/main/java/frc/robot/subsystems/DriverViewSubsystem.java
@@ -7,11 +7,13 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.cscore.HttpCamera;
 import edu.wpi.cscore.VideoSource;
+import edu.wpi.cscore.HttpCamera.HttpCameraKind;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.Limelight;
 
@@ -21,28 +23,37 @@ public class DriverViewSubsystem extends SubsystemBase
      * Creates a new DriverViewSubsystem.
      */
     private ShooterSubsystem m_Shooter;
-    private ShuffleboardTab tab;
+    private ShuffleboardTab tab = Shuffleboard.getTab("Driver View");
     private TurretSubsystem m_Turret;
     private HopperSubsystem m_Hopper;
     private VideoSource m_Limelight;
 
+    private NetworkTableEntry m_shooter_velocity = tab.add("Shooter Velocity", 0).getEntry();
+    private NetworkTableEntry m_turret_angle = tab.add("Turret Angle", 0).getEntry();
+    private NetworkTableEntry m_beam_break_bot = tab.add("Bottom Beam Status", 0).getEntry();
+    private NetworkTableEntry m_beam_break_top = tab.add("Top Beam Status", 0).getEntry();
+
     public DriverViewSubsystem(ShooterSubsystem p_Shooter, TurretSubsystem p_Turret, HopperSubsystem p_Hopper)
     {
-        tab = Shuffleboard.getTab("Driver View");
         m_Shooter = p_Shooter;
         m_Turret = p_Turret;
         m_Hopper = p_Hopper;
-        m_Limelight = CameraServer.getInstance().getVideo("limelight").getSource();
+
+        HttpCamera limelightFeed = new HttpCamera("limelight", "http://limelight.local:5800/stream.mjpg",
+            HttpCameraKind.kMJPGStreamer);
+        // HttpCamera limelightFeed = new HttpCamera("limelight", "http://10.3.22.11:5800/stream.mjpg", HttpCameraKind.kMJPGStreamer);
+        CameraServer.getInstance().startAutomaticCapture(limelightFeed);
+        // m_Limelight = CameraServer.getInstance().getVideo("limelight").getSource();
     }
 
     @Override
     public void periodic()
     {
         // This method will be called once per scheduler run
-        tab.add("Shooter Velocity", m_Shooter.getVelocity());
-        tab.add("Turret Angle", m_Turret.getTurretAngle());
-        tab.add("Bottom Beam Status", m_Hopper.bottomBeamStatus());
-        tab.add("Top Beam Status", m_Hopper.topBeamStatus());
-        tab.add(m_Limelight);
+        m_shooter_velocity.setDouble(m_Shooter.getVelocity());
+        m_turret_angle.setDouble(m_Turret.getTurretAngle());
+        m_beam_break_bot.setBoolean(m_Hopper.bottomBeamStatus());
+        m_beam_break_top.setBoolean(m_Hopper.topBeamStatus());
+        // tab.add(m_Limelight);
     }
 }
